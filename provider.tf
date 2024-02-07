96bba139e42e4c340830302a0b7316be37e1edea
vamsi52002 <147084719+vamsi52002@users.noreply.github.com>
2024-02-07 15:29:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # Configuration options
   region = "us-west-1"
-  access_key =  "AKIA3A54I3DAVW7H7IWR"
-  secret_key = "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"
+  access_key =  "AKIA3A54I3DAWTALRAS4"
+  secret_key = "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"
 }
