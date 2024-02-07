571d023293cc4cf4f5ff9e891e7e710f4a66cdb2
vamsi52002 <147084719+vamsi52002@users.noreply.github.com>
2024-02-07 15:50:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "exampel" {
-  bucket = "vam123456"
+bucket = "vam123456"
 
     tags = {
-    Name        = "My bucket"
-    Environment = "jenkins"
+Name        = "My bucket"
+Environment = "jenkins"
   }
 }
